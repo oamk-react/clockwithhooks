4e59d06708f5f08ed93a96a9afb63e3a71ebe758
Jouni Juntunen <jouni.juntunen@oamk.fi>
2020-11-06 15:13:08 +0200

    Example with useEffect.

---
+++ src/Clock.js
@@ -0,0 +1,22 @@
+import React,{useState, useEffect} from 'react'
+
+export default function Clock() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    console.log('componentdidmount');
+    const id = setInterval(() => {
+      setTime(new Date());
+    },1000);
+    return () => {
+      console.log('componentwillunmount');
+      clearInterval(id);
+    };
+  },[])
+
+  return (
+    <div style={{margin: '30px'}}>
+      {time.toLocaleTimeString()}
+    </div>
+  )
+}
